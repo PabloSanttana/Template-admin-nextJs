ab6650320bfcd22cdcce3b437eb54e570849c64d
PabloSanttana <pablo_guilherme02@hotmail.com>
2021-11-06 12:46:00 -0300

    sjuste de label

--- src/components/template/SideBar.tsx
+++ src/components/template/SideBar.tsx
@@ -6,6 +6,13 @@ interface SideBarProps {}
 export default function SideBar(props: SideBarProps) {
   return (
     <aside>
+      <div
+        className={`
+        flex flex-col items-center justify-center
+        h-20 w-20
+        bg-gradient-to-r from-indigo-500 to-purple-500
+      `}
+      ></div>
       <ul>
         <HeaderItem url="/" text="Início" icon={IconHome} />
         <HeaderItem url="/ajuste" text="Ajuste" icon={IconSetting} />
--- src/components/template/Title.tsx
+++ src/components/template/Title.tsx
@@ -8,14 +8,18 @@ export default function Title(props: TitleProps) {
     <div>
       <h1
         className={`
-          
+          font-black
+          text-3xl
+          text-gray-900  
       `}
       >
         {props.title}
       </h1>
       <h2
         className={`
-      
+        font-light
+        text-sm
+        text-gray-600
       `}
       >
         {props.subTitle}
